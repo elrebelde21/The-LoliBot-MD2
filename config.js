4d16dc2d47c40e9946253edf6f93889516f7a4cd
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-06-22 19:07:23 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -85,7 +85,7 @@ global.version = vs
 global.gt = 'The LoliBot-MD'
 global.gatabot = gt
 
-global.yt = 'https://youtube.com/channel/UC9b3UIVnVb5eQt7_Y-Qz-3A''
+global.yt = 'https://youtube.com/channel/UC9b3UIVnVb5eQt7_Y-Qz-3A'
 global.youtube = yt
 
 global.ig = ''
@@ -97,7 +97,7 @@ global.gatabot = md
 global.nn = 'https://chat.whatsapp.com/Lus9S60MABnH9lF4Wf2T7k'
 global.nngrupo = nn
 
-global.nnn = 'https://chat.whatsapp.com/DCJclB8oBAPIAoleUtNEaN'
+global.nnn = 'https://chat.whatsapp.com/CFSvSOGCnuL3vk4Js1DWI2'
 global.nnngrupo = nnn
 
 global.paypal = 'https://www.instagram.com/gata_dios  https://paypal.me/OficialGD'
