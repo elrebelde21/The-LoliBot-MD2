6eb762f4878760ba3f66d5183230ad71435941f4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-07-23 22:49:52 -0300

    Add files via upload

--- handler.js
+++ handler.js
@@ -555,8 +555,6 @@ export async function handler(chatUpdate) {
                             console.error(e)
                         }
                     }
-                    if (m.limit)
-                        m.reply(+m.limit + ' 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀(𝙎) 💎 𝙐𝙎𝘼𝘿𝙊(𝙎)')
                 }
                 break
             }
