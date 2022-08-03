694d73a965a71f52d3401b006de1757133ad9709
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-08-02 21:23:59 -0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -21,6 +21,7 @@ let handler = async (m, { conn }) => {
   m.reply(`${eg}𝑬𝒔𝒕𝒆 𝒄𝒉𝒂𝒕 𝒇𝒖𝒆 𝒅𝒆𝒔𝒃𝒂𝒏𝒆𝒂𝒅𝒐🥳\n𝑨𝒉𝒐𝒓𝒂 𝒆𝒔𝒕𝒐𝒚 𝒅𝒊𝒔𝒑𝒐𝒏𝒊𝒃𝒍𝒆..`)
 }
 handler.command = /^unbanchat$/i
+handler.rowner = true
 handler.botAdmin = true
 handler.admin = true
 
