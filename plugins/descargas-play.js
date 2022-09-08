8e747f586fbc40c6138b571b010fa3869798b3f1
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-09-08 20:30:27 -0300

    Add files via upload

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,26 +1,37 @@
-import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `🤔𝙦𝙪𝙚 𝙚𝙨𝙩𝙖 𝙗𝙪𝙨𝙘𝙖𝙙𝙤?🤔 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚/𝙩𝙞𝙩𝙪𝙡𝙤 𝙙𝙚 𝙘𝙖𝙣𝙘𝙞𝙤𝙣\n\n*—◉ 𝙀𝙟𝙚𝙢𝙥𝙡𝙤:*\n*#play bad Bunny - am*`
-let vid = (await youtubeSearch(text)).video[0]
-if (!vid) throw '*[❗𝙄𝙣𝙛𝙤❗] 𝙇𝙤𝙨 𝙨𝙞𝙚𝙣𝙩𝙤, 𝙣𝙤 𝙥𝙪𝙙𝙚 𝙚𝙣𝙘𝙤𝙣𝙩𝙧𝙖𝙧 𝙚𝙡 𝙖𝙪𝙙𝙞𝙤/𝙫𝙞𝙙𝙚𝙤, 𝙞𝙣𝙩𝙚𝙣𝙩𝙚 𝙘𝙤𝙣 𝙤𝙩𝙧𝙤𝙨 𝙣𝙤𝙢𝙗𝙧𝙚𝙨/𝙩𝙞𝙩𝙪𝙡𝙤𝙨'
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, {command, conn, text, usedPrefix }) => {
+if (!text) throw `${mg}🤔𝙦𝙪𝙚 𝙚𝙨𝙩𝙖 𝙗𝙪𝙨𝙘𝙖𝙙𝙤?🤔 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚/𝙩𝙞𝙩𝙪𝙡𝙤 𝙙𝙚 𝙘𝙖𝙣𝙘𝙞𝙤𝙣\𝙀𝙟𝙚𝙢𝙥𝙡𝙤\n*${usedPrefix + command} Bad Bunny - am*`
 try {
-let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-const url = 'https://www.youtube.com/watch?v=' + videoId
-conn.sendHydrated(m.chat, `
-📌 *𝗧𝗶𝘁𝘂𝗹𝗼:* ${title}
-📇 *𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼𝗻:* ${description}
-📆 *𝗣𝘂𝗯𝗹𝗶𝗰𝗮𝗰𝗶𝗼𝗻:* ${publishedTime}
-⌚ *𝗗𝘂𝗿𝗮𝗰𝗶𝗼𝗻:* ${durationH}
-👀 *𝗩𝗶𝘀𝘁𝗮𝘀:* ${viewH}
-`.trim(), author, thumbnail, `${url}`, '𝚄𝚁𝙻', null, null, [
-['𝐀𝐔𝐃𝐈𝐎', `${usedPrefix}yta ${url} yes`],
-['𝐕𝐈𝐃𝐄𝐎', `${usedPrefix}ytv ${url} yes`]
-], m)
+if (command == 'play') {
+conn.reply(m.chat, `${eg}⏳Procesando⏳`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: 'The Lolibot-MD',
+body: 'Super Bot WhatsApp',         
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`}}}) 
+  
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
+//https://leyscoders-api.herokuapp.com/api/playmp3?q=lebih%20baik%20darinya&apikey=Your_Key
+  //("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.url, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
+if (command == 'play2') {
+conn.reply(m.chat, `${eg}⏳Procesando⏳`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',
+body: '𝙎𝙪𝙥𝙚𝙧 𝙗𝙤𝙩 𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥',         
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`}}})
+  
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text) 
+let json = await res.json()
+conn.sendFile(m.chat, json.result.url, 'error.mp4', `${wm}`, m)}
 }catch(e){
-m.reply('*[❗𝙄𝙣𝙛𝙤❗] 𝑬𝒓𝒓𝒐𝒓, 𝒑𝒐𝒓 𝒇𝒂𝒗𝒐𝒓 𝒗𝒖𝒆𝒍𝒗𝒂 𝒂 𝒊𝒏𝒕𝒆𝒏𝒕𝒂𝒓𝒍𝒐')
+m.reply(`${fg}𝙄𝙣𝙩𝙚𝙣𝙩𝙚 𝙙𝙚 𝙣𝙪𝙚𝙫𝙤𝙨`)
 console.log(e)
 }}
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.help = ['play' , 'play2'].map(v => v + ' <texto>')
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
+handler.command = ['play', 'play2']
 export default handler
