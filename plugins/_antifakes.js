9b73eae1d20008f4108c3e8b8e66276c06a40d5e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-09-18 12:03:28 -0300

    Add files via upload

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -9,61 +9,61 @@ if (isBotAdmin) {
 if (m.sender.startsWith('91' || '91')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😄 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 		
 if (m.sender.startsWith('92' || '92')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😯 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 ')
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('93' || '93')) {
+global.db.data.users[m.sender].banned = true
+await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😯 ')
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('234' || '234')) {
+global.db.data.users[m.sender].banned = true
+await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😯 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('27' || '27')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😯 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('212' || '212')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😄 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('210' || '210')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😯 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('61' || '61')) {
 global.db.data.users[m.sender].banned = true
 await m.reply(' Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 🤨')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 
 if (m.sender.startsWith('49' || '49')) {
 global.db.data.users[m.sender].banned = true
 await m.reply('  Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 🤨 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 	
 if (m.sender.startsWith('20' || '20')) {
 global.db.data.users[m.sender].banned = true
 await m.reply('  Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😄 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 	
 if (m.sender.startsWith('62' || '62')) {
 global.db.data.users[m.sender].banned = true
 await m.reply('  Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 🤨 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 🤑 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 	
 if (m.sender.startsWith('40' || '40')) {
 global.db.data.users[m.sender].banned = true
 await m.reply('  Antifake activado en este grupo, lo siento seras expulsado.. tu numero parece algo fake 😄 ')
-await m.reply(' 𝑬𝒙𝒑𝒖𝒍𝒔𝒂𝒓 𝒂𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 ')
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
 }
 }
