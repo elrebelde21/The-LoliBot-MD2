0786063827f00f9d023c5c565d2cb1dcf36d03e4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-09-20 21:18:24 -0300

    Add files via upload

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -13,9 +13,14 @@ this.spam[m.sender].spam += 1
 if (new Date - this.spam[m.sender].lastspam > 4000) {
 if (this.spam[m.sender].spam > 6) {
 this.spam[m.sender].spam = 0
+let chat = global.db.data.chats[m.chat]
+let delet = m.key.participant
+let bang = m.key.id
+let bot = global.db.data.settings[this.user.jid] || {}
 this.spam[m.sender].lastspam = new Date * 1
 //global.DATABASE._data.users[m.sender].Banneduser = true
 m.reply('No hagas Spam, anda hacer spam a la concha de tu hermana 🤨!!')
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await this.updateBlockStatus(m.chat, 'block')
 } else {
 this.spam[m.sender].spam = 0
