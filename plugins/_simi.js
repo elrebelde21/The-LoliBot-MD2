9d8af97815be09c3d846a8821324a10670111726
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-09-22 10:28:59 -0300

    Add files via upload

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -1,10 +1,12 @@
-export async function before(m) {
-    let chat = db.data.chats[m.chat]
-    if (chat.simi && !chat.isBanned && !m.fromMe) {
-    	if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+import fetch from 'node-fetch'
+let handler = m => m
+
+handler.before = async (m) => {
+    let chat = global.db.data.chats[m.chat]
+    if (chat.simi) {
+        if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
         let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
-        if (!ressimi.ok) throw 'no entendi  :v'
         let data = await ressimi.json();
         if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') await m.reply("No habla taka taka x'd")
         await m.reply(data.success)
@@ -12,3 +14,4 @@ export async function before(m) {
     }
     return true
 }
+export default handler
