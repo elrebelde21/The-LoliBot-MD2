9deb93e02ecd8eb38078a6a31408d0932348ca69
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-10-01 13:44:36 -0300

    Add files via upload

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let txt = ''
 let vn = './media/listas.mp3'
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n🐈 ${await conn.getName(jid)}\n🔸 ${jid} \n${chat?.metadata?.read_only ? '❌ *𝑺𝒊𝒏 𝒆𝒔𝒕𝒂𝒓 𝒂𝒒𝒖𝒊 | 𝑵𝒐*' : '✅ *𝑺𝒊𝒈𝒐 𝒂𝒒𝒖𝒊 | 𝒀𝒆𝒔*'}\n\n`
-m.reply(`*${gt} ESTÁ EN ESTOS GRUPOS:*`.trim())
+m.reply(`*${gt} 𝑬𝒔𝒕𝒂 𝒆𝒏 𝒆𝒔𝒕𝒐𝒔 𝒈𝒓𝒖𝒑𝒐𝒔:*`.trim())
 
 conn.sendHydrated(m.chat, txt, wm, null, 'https://github.com/elrebelde21/The-LoliBot-MD', '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫', null, null, [
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', '.menu'],
