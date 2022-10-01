ba2b9e61438fd106954fbf04c2c328fad12c6b91
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-10-01 14:34:44 -0300

    Add files via upload

--- plugins/lista-premium.js
+++ plugins/lista-premium.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, isOwner }) => {
 let vip = global.db.data.users[m.sender].premium
 let prem = Object.entries(global.db.data.users).filter(user => user[1].premium)
-let caption = `🎟️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈
+let caption = `🎟️ 𝑼𝒔𝒖𝒂𝒓𝒊𝒐 𝑷𝒓𝒆𝒎𝒊𝒖𝒎
 *╭•·–––––––––––––––––––·•*
-│ *Total : ${prem.length} Usuarios* ${prem ? '\n' + prem.map(([jid], i) => `
+│ *𝑻𝒐𝒕𝒂𝒍 : ${prem.length} 𝑼𝒔𝒖𝒂𝒓𝒊𝒐* ${prem ? '\n' + prem.map(([jid], i) => `
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios' : conn.getName(jid)}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
 *╰•·–––––––––––––––––––·•*`
-await conn.sendButton(m.chat, caption, `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${vip ? '✅' : '❌'}\n${wm}`, null, [ 
-[`${vip ? '✦ 𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦': '✦ 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 𝙋𝘼𝙎𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦'}`, `${vip ? '.allmenu': '.pase premium'}`]], m, { mentions: await conn.parseMention(caption) })
+await conn.sendButton(m.chat, caption, `🎟️ 𝒑𝒓𝒆𝒎𝒊𝒖𝒎  ⇢ ${vip ? '✅' : '❌'}\n${wm}`, null, [ 
+[`${vip ? '✦ 𝑫𝒊𝒔𝒇𝒓𝒖𝒕𝒂 𝒅𝒆𝒍 𝒑𝒓𝒆𝒎𝒊𝒖𝒎 ✦': '✦ 𝑪𝒐𝒎𝒑𝒓𝒂 𝒑𝒂𝒔𝒆 𝒑𝒓𝒆𝒎𝒊𝒖𝒎✦'}`, `${vip ? '.allmenu': '.pase premium'}`]], m, { mentions: await conn.parseMention(caption) })
 }
 handler.command = /^(listapremium)$/i
 
