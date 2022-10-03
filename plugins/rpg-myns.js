7871f4fadd670f47541840c64ff473d67413edd9
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-10-02 21:01:16 -0300

    Add files via upload

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`⬇️ *ESE ES SU NUMERO DE SERIE* ⬇️`.trim())
+await m.reply(`⬇️ *𝒆𝒔𝒆 𝒆𝒔 𝒔𝒖 𝒏𝒖́𝒎𝒆𝒓𝒐 𝒅𝒆𝒍 𝒔𝒆𝒓𝒊𝒆* ⬇️`.trim())
 await m.reply(`${sn}`.trim())
 }
 handler.help = ['myns']
