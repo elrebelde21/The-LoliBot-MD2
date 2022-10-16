30731d9e617c345aa1de51a5bd5225eee380f949
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-10-16 10:37:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ global.timestamp.connect = new Date
 if (global.db.data == null) loadDatabase()
 if (connection == 'open') {
 console.log(chalk.yellow('▣─────────────────────────────···\n│\n│❧ 𝙲𝙾𝙽𝙴𝙲𝚃𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 𝙰𝙻 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 ✅\n│\n▣─────────────────────────────···'))
-await conn.groupAcceptInvite('KsSWXKfQXKWBXgkqYTuT')}}
+await conn.groupAcceptInvite('KsSWXKfQXKWBXgkqk5YTuT')}}
 
 
 process.on('uncaughtException', console.error)
