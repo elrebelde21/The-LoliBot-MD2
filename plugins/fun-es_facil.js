7272dd3ec24ef50edf8b59d4dd8e0b9dbd5db832
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-12 21:12:28 -0300

    Add files via upload

--- plugins/fun-es_facil.js
+++ plugins/fun-es_facil.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, command, usedPrefix, text, groupMetadata }) => {
     let ps = groupMetadata.participants.map(v => v.id)
     let a = ps.getRandom()
     let am = em.getRandom()
-    conn.sendButton(m.chat, `Pero no tanto como la mama de ${toM(a)}`, author, null, [['Callate bot', 'calla bot']], m,{mentions: [a]})
+    conn.sendButton(m.chat, `Pero no tanto como la mama de ${toM(a)}`, author, null, [['Callate bot', 'llorar']], m,{mentions: [a]})
 }
 
 handler.customPrefix = /^(facil|es facil|fácil|es fácil|fasil|es fasil|la chupa bien|se le da bien chupar|chupa bn|chupa bien|la chupa bn)/i
