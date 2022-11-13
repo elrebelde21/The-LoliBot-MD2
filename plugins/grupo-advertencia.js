e94c6093721f8bd790e46eab6b0b9ffecbbb5bf6
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-13 19:07:37 -0300

    Add files via upload

--- plugins/grupo-advertencia.js
+++ plugins/grupo-advertencia.js
@@ -1,45 +1,37 @@
 let handler = async (m, { conn, text, command, usedPrefix }) => {//prems 
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-}  
-if (!db.data.chats[m.chat].antitoxic && m.isGroup) return conn.reply(m.chat, `𝙇𝙖 𝙛𝙪𝙣𝙘𝙞𝙤́𝙣 *#on antitoxicos* 𝙚𝙨𝙩𝙖́ 𝘿𝙚𝙨𝙖𝙘𝙩𝙞𝙫𝙖𝙗𝙖, 𝙙𝙚𝙗𝙚 𝙙𝙚 𝙚𝙨𝙩𝙖 𝙖𝙘𝙩𝙞𝙫𝙖 𝙙𝙞𝙘𝙝𝙖 𝙛𝙪𝙣𝙘𝙞𝙤́𝙣, 𝙥𝙖𝙧𝙖 𝙦𝙪𝙚 𝙛𝙪𝙣𝙘𝙞𝙤𝙣𝙚 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤`, fkontak, m) 
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+if (!db.data.chats[m.chat].antitoxic && m.isGroup) return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 *#on antitoxicos* 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝘼, 𝘿𝙀𝘽𝙀 𝘿𝙀 𝘼𝘾𝙏𝙄𝙑𝘼𝙍 𝘿𝙄𝘾𝙃𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝙍 𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊`, fkontak, m) 
+
 let who
-let img = 'https://telegra.ph/file/635b82df8d7abb4792eab.jpg'
+let img = 'https://i.imgur.com/DvHoMc3.jpg'
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
 let user = global.db.data.users[who]
-if (!who) throw `𝑬𝒕𝒊𝒒𝒖𝒆𝒕𝒂𝒔 𝒂𝒍 𝒑𝒆𝒓𝒔𝒐𝒏𝒂 𝒒𝒖𝒆 𝒗𝒂𝒏 𝒓𝒆𝒄𝒊𝒃𝒊𝒓 𝒖𝒏𝒂 𝒂𝒅𝒗𝒆𝒓𝒕𝒆𝒏𝒄𝒊𝒂 ⚠️\n\n𝒆𝒋𝒆𝒎𝒑𝒍𝒐 \n*${usedPrefix + command} @tag*`
+if (!who) throw `${lenguajeGB['smsAvisoMG']()}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙌𝙐𝙀 𝙑𝘼 𝙍𝙀𝘾𝙄𝘽𝙄𝙍 𝙇𝘼 𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 ⚠️\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag Motivo*`
+let txt = text.replace('@' + who.split`@`[0], '').trim()
+if (!txt) throw `${lenguajeGB['smsAvisoMG']()}𝘿𝙀𝘽𝙀 𝘿𝙀 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝙐𝙉 𝙈𝙊𝙏𝙄𝙑𝙊 𝙋𝘼𝙍𝘼 𝘼𝘿𝙑𝙀𝙍𝙏𝙄𝙍\n\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag Motivo*`
+try {
 user.warn += 1
-  
-await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝑹𝒆𝒄𝒊𝒃𝒊𝒐́ 𝒖𝒏𝒂 𝒂𝒅𝒗𝒆𝒓𝒕𝒆𝒏𝒄𝒊𝒂 𝒆𝒏 𝒆𝒔𝒕𝒆 𝒈𝒓𝒖𝒑𝒐!!\n\n`, `*𝒂𝒅𝒗𝒆𝒓𝒕𝒆𝒏𝒄𝒊𝒂:*\n⚠️ *${user.warn}/4*\n\n${wm}`, img, [
-[`😭 𝑳𝒐 𝒔𝒊𝒆𝒏𝒕𝒐`, '.ok'],
-['☘️ 𝑴𝒆𝒏𝒖', '/menu']], false, { mentions: [who] }) //[m.sender]
+
+await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝙍𝙀𝘾𝙄𝘽𝙄𝙊 𝙐𝙉𝘼 𝘼𝘿𝙑𝙀𝙉𝙏𝙀𝙉𝘾𝙄𝘼 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊!!\n\n🫵 *${text}*`, `*ADVERTENCIA*\n⚠️ *${user.warn}/4*\n\n${wm}`, img, [
+[`😭 𝙇𝙊 𝙎𝙄𝙀𝙉𝙏𝙊`, '.ok'],
+['☘️ 𝙈𝙀𝙉𝙐', '/menu']], false, { mentions: [who] }) //[m.sender]
 	
 if (user.warn >= 4) {
 user.warn = 0
-await m.reply(`𝙏𝙚 𝙡𝙤𝙨 𝙖𝙙𝙫𝙚𝙧𝙩𝙞𝙙 𝙫𝙖𝙧𝙞𝙖𝙨 𝙫𝙚𝙘𝙚𝙨!!\n*@${who.split`@`[0]}* 𝙨𝙪𝙥𝙚𝙧𝙖𝙧𝙩𝙚 𝙡𝙖𝙨 *4* 𝙖𝙙𝙫𝙚𝙧𝙩𝙚𝙣𝙘𝙞𝙖\n 𝙖𝙝𝙤𝙧𝙖 𝙨𝙚𝙧𝙖́ 𝙚𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤(𝘼) 😐`, false, { mentions: [who] })
+await m.reply(`𝙏𝙀 𝙇𝙊 𝘼𝘿𝙑𝙀𝙍𝙏𝙄𝘿 𝙑𝘼𝙍𝙄𝘼𝙎 𝙑𝙀𝘾𝙀𝙎!!\n*@${who.split`@`[0]}* 𝙎𝙐𝙋𝙀𝙍𝘼𝙎𝙏𝙀 𝙇𝘼𝙎 *4* 𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼𝙎 𝘼𝙃𝙊𝙍𝘼 𝙎𝙀𝙍𝘼𝙎 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝘿𝙊(𝘼) 🙄`, false, { mentions: [who] })
 user.banned = true
 await conn.groupParticipantsUpdate(m.chat, [who], 'remove') //@${m.sender.split`@`[0]}
 //await this.updateBlockStatus(m.sender, 'block')
 }
 return !1
-}
+} catch (e) {
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}*ALGO SALIÓ MAL.*\n𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 *${usedPrefix + command} @tag Motivo*\n\n\`\`\`REPORTE ESTE COMANDO ${usedPrefix + command} CON EL COMANDO ${usedPrefix}reporte\`\`\``, m)
+}}
 handler.help = ['addprem <@user>']
 handler.tags = ['owner']
 handler.command = /^(advertir|advertencia|warn|warning)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
-handler.register = true
 export default handler
