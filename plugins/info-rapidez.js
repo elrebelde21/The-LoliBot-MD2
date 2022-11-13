a14244aee39ce386d080a430074966ce2716c2d9
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-13 19:02:16 -0300

    Add files via upload

--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -1,7 +1,45 @@
-let handler = async m => {
- let old = performance.now()
- let neww = performance.now()
- let speed = neww - old
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import util from 'util'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+let format = sizeFormatter({
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn, usedPrefix, command }) => {
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+  let old = performance.now()
+  
+  let neww = performance.now()
+  let speed = neww - old
  let txt = `*_𝘾𝙤𝙢𝙚𝙣𝙯𝙖𝙣𝙙𝙤 𝙥𝙧𝙪𝙚𝙗𝙖 𝙙𝙚𝙡 𝙫𝙚𝙡𝙤𝙘𝙞𝙙𝙖𝙙..._*`.trim()
   m.reply(txt)
 
@@ -10,30 +48,31 @@ await m.reply('🚀🚀')
 await m.reply('🚀🚀🚀')
 await m.reply(`╰⊱✅⊱ *𝑹𝒆𝒔𝒖𝒍𝒕𝒂𝒅𝒐𝒔* ⊱✅⊱╮`)
  
-let veloz = 
-`🚄 *𝑽𝒆𝒍𝒐𝒄𝒊𝒅𝒂𝒅:*\n *${speed}* *Milisegundos*\n\n🚄 *𝑺𝒑𝒆𝒆𝒅:*\n *${speed}* *Milliseconds*`
+let infobt = `
+🚄 *𝑽𝒆𝒍𝒐𝒄𝒊𝒅𝒂𝒅:*\n *${speed}* *Milisegundos*\n\n🚄 *𝑺𝒑𝒆𝒆𝒅:*\n *${speed}* *Milliseconds*
 
- const templateButtonsReplyMessage = [
-{index: 1, urlButton: {displayText: '𝙶𝙸𝚃𝙷𝚄𝙱', url: 'https://github.com/elrebelde21/The-LoliBot-MD'}},
-{index: 2, urlButton: {displayText: 'YouTube', url: 'https://www.youtube.com/@TheLoliBot-MD'}},
-{index: 3, quickReplyButton: {displayText: '𝙊𝙩𝙧𝙖 𝙫𝙚𝙯', id: '#ping'}},
-{index: 4, quickReplyButton: {displayText: '𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣', id: '#infobot'}},
-{index: 5, quickReplyButton: {displayText: '𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́', id: '#menu'}},
-]
-let tm = {
-text: veloz,
-footer: global.wm,
-templateButtons: templateButtonsReplyMessage
+≡ *INFO BOT*
+  
+*ESTADO*
+▢ *${groupsIn.length}* Chats de grupo
+▢ *${groupsIn.length}* Gropos unidos
+▢ *${groupsIn.length - groupsIn.length}* Grupos abandonados
+▢ *${chats.length - groupsIn.length}* Chats privados
+▢ *${chats.length}* Total Chats
+
+ *≡ S E R V E R*
+*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+*🔵 FreeRAM:* ${format(freemem())}
+
+*≡  NodeJS Uso de memoria*
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+`
+conn.sendButton(m.chat, infobt, igfg, null, [
+  ['🔰 menu', `${usedPrefix}menu`],
+   ['❇️ Grupos', `${usedPrefix}grupos`]
+ ], m)
+
 }
-conn.sendMessage(m.chat, tm, m)
-}
-// let veloz = `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`.trim() 
-//conn.sendButton(m.chat, `${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`, wm, veloz, [['Vista', /${command}]], m)
-                                               
-//m.reply(`${rg}*VELOCIDAD:* *${speed}* *Milisegundos*\n*SPEED:* *${speed}* *Milliseconds*`)
-
- 
-
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = /^(ping|speed|velocidad|rapidez|velocity)$/i
