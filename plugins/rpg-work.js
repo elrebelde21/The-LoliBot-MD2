22da9b1211e16602a5bfe5c577bf89b26ac09ee4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-12 21:44:06 -0300

    Add files via upload

--- plugins/rpg-work.js
+++ plugins/rpg-work.js
@@ -1,23 +1,15 @@
 // creditos a https://github.com/FG98F
 let handler = async (m, { conn, isPrems}) => {
-//let hasil = Math.floor(Math.random() * 5000)
-let pp = 'https://c4.wallpaperflare.com/wallpaper/991/456/22/sketch-artist-anime-anime-girls-arknights-swire-arknights-hd-wallpaper-preview.jpg'
-let gata = Math.floor(Math.random() * 3000)
-global.db.data.users[m.sender].exp += gata * 1  
+let hasil = Math.floor(Math.random() * 5000)
 let time = global.db.data.users[m.sender].lastwork + 600000
-if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*Ya trabajó �? espere unos ${msToTime(time - new Date())} para volver a trabajar!!*`
+if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*𝙴𝚜𝚝𝚊𝚜 𝚌𝚊𝚗𝚜𝚊𝚍𝚘, 𝚍𝚎𝚋𝚎𝚜 𝚍𝚎𝚜𝚌𝚊𝚗𝚜𝚊𝚛 𝚌𝚘𝚖𝚘 𝚖𝚒𝚗𝚒𝚖𝚘 ${msToTime(time - new Date())} 𝚙𝚊𝚛𝚊 𝚟𝚘𝚕𝚟𝚎𝚛 𝚊 𝚝𝚛𝚊𝚋𝚊𝚓𝚊𝚛!*`
 
-//m.reply(`${pickRandom(global.work)} *${gata} XP*`)
-conn.sendHydrated(m.chat, wm, `${pickRandom(global.work)} ${gata} XP`, pp, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `.menu`]
-], m,) 
- 
+m.reply(`${pickRandom(global.work)} *${hasil} XP*`)
 global.db.data.users[m.sender].lastwork = new Date * 1
 }
 handler.help = ['work']
 handler.tags = ['xp']
 handler.command = ['work', 'trabajar']
-handler.register = true
 handler.fail = null
 handler.exp = 0
 export default handler
@@ -39,7 +31,7 @@ function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]
 }
 
-global.work = ["Trabajas como cortador de galletas y ganas", "Trabaja para una empresa militar privada, ganando", "Organiza un evento de cata de vinos y obtiene",
+global.work = ["Trabajas como cortador de galletas y ganas", "cuidarte el grupos del lolibot ganar  450 xp", "defendiste al boxmine del ataque del los ni?o rata", "trabaja el un boliche todas las noche ganarte una piba? y 450 xp", "le hiciste la paja un hombre por 150 xp", "Trabaja para una empresa militar privada, ganando", "Organiza un evento de cata de vinos y obtiene",
  "Te secuestran y te llevan a un coliseo subterráneo donde luchaste contra monstruos con personas que nunca antes habías conocido. Ganas", "Limpias la chimenea y encuentras", 
 "Desarrollas juegos para ganarte la vida y ganas", 
 "¿Por qué este comando se llama trabajo? Ni siquiera estás haciendo nada relacionado con el trabajo. Sin embargo, ganas", "Trabajaste en la oficina horas extras por", 
@@ -57,5 +49,6 @@ global.work = ["Trabajas como cortador de galletas y ganas", "Trabaja para una e
 "Trabajas como podador de arbustos para *FG98* y ganas", "La demanda de juegos para dispositivos móviles ha aumentado, por lo que creas un nuevo juego lleno de micro-transacciones. Con tu nuevo juego ganas un total de", 
 "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar", 
 "Estabas cultivando y Ganaste", "Trabajas como constructor de castillos de arena y ganas", "Trabajaste y Ganaste", 
-"Trabajas como artista callejera y ganas","¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste"
+"Trabajas como artista callejera y ganas","¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste", 
+"Llevas mujeres a la tienda por"
 ]
