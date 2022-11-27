27abfd93a8223a88e283672341983897363101b8
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-27 17:18:12 -0300

    Add files via upload

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -13,9 +13,9 @@ while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
 	  	 
 conn.sendButton(m.chat, ' ' + wm, `┌───⊷ ${lenguajeGB.smsAutoLv1()}
-▢ ${lenguajeGB.smsAutoLv2()} ${before}*
-▢ ${lenguajeGB.smsAutoLv3()} ${user.level}*
-▢ ${lenguajeGB.smsAutoLv4()}* ${user.role}
+▢ ${lenguajeGB.smsAutoLv2()} ${before}
+▢ ${lenguajeGB.smsAutoLv3()} ${user.level}
+▢ ${lenguajeGB.smsAutoLv4()} ${user.role}
 └──────────────
 
 *_${lenguajeGB.smsAutoLv6()}_*`, null, [[lenguajeGB.smsConMenu(), `/menu`]], m)
