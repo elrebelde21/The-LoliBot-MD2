d9bff62dd85137da2e7c9ccaf254cecea41e3f01
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-11-28 14:31:17 -0300

    Add files via upload

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,7 +5,7 @@ let text = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤* 
 *Wa.me/5492266466080 (no bot)*
 *wa.me/51984498676 (bot)*
-*wa.me/573183650526 (bot²)*
+*wa.me/529612005844 (bot²)*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
