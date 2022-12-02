f36ff948ccaa03f93db5df66404259adbe760d05
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-02 14:22:51 -0300

    Update propietario(a)-notificar.js

--- plugins/propietario(a)-notificar.js
+++ plugins/propietario(a)-notificar.js
@@ -1,6 +1,6 @@
 import fs from 'fs' 
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-const grupo = nn
+const grupo = nnnttt
 
 let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
 let users = m.sender.split`@`[0]
