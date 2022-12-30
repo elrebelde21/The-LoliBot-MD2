cc8a1374e78069c6a92d542e85259632fb2c199c
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-30 18:18:55 -0300

    Add files via upload

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,5 +1,9 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+let user = global.db.data.users[m.sender]
+  let time = user.prueb + 40000 //tiempo de espera en min
+if (new Date - user.prueb < 40000) return await conn.reply(m.chat, `*𝙀𝙨𝙥𝙚𝙧𝙖 𝙪𝙣𝙤𝙨 𝙨𝙚𝙜𝙪𝙣𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙤𝙩𝙧𝙤 𝙘𝙤𝙢𝙖𝙣𝙙𝙤*`,  m)
+
 if (!text) throw `𝗤𝘂𝗲 𝗯𝘂𝘀𝗰𝗮𝗿? 🤔️\n 𝗨𝘀𝗲𝗿 𝙙𝙚 𝙡𝙖 𝙨𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 𝙢𝙖𝙣𝙚𝙧𝙖\n𝗘𝗷𝗲𝗺𝗽𝗹𝗼\n*${usedPrefix + command} Loli*`
 const res = await googleImage(text)
 let image = res.getRandom()
@@ -13,3 +17,16 @@ handler.tags = ['internet', 'tools']
 handler.command = /^(gimage|image|imagen)$/i
 handler.exp = 20
 export default handler
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+
+return minutes + " m y " + seconds + " s " 
+}
