01252d46a1ffb9e59d8ab97774793a2628a01697
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-02 21:01:49 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -4,8 +4,8 @@ export async function before(m, { isAdmin, isBotAdmin, isOwner, isROwner }) {
     if (m.isGroup)
        return !1
     if (!m.message)
-       return !0
-    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA'))
+       return !0<
+   if (m.text.includes('PIEDRA') || m.text.includes('hola')   || m.text.includes('estado') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0
        return !0
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
