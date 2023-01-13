2ddc0c626c68e87b79a7f0b85adbf8ac5e584566
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-13 12:17:39 -0300

    Update _antispam_.js

--- plugins/_antispam_.js
+++ plugins/_antispam_.js
@@ -8,7 +8,7 @@ return
 if (!user.antispam)
 return !0
 if (+new Date() > user.antispam) {
-let tiempo = 30000 * 5
+let tiempo = 1200000 * 20
 setTimeout(() => {
 //this.updateBlockStatus(m.chat, 'unblock') No me gustó jjjjj
 user.banned = false
