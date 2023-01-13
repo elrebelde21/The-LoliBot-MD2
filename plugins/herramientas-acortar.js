b8af7dc02d83a9acccbf0f5b6219a617ce4599bc
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-13 19:55:35 -0300

    Add files via upload

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -1,12 +1,11 @@
-import axiostal from "axios"
+
 import fetch from 'node-fetch'
-let handler = async(m, { conn, text, xteamkey }) => {
-if (!text) throw `${mg} 𝙔 𝙚𝙡 𝙚𝙣𝙡𝙖𝙘𝙚?\n 𝙞𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙣𝙡𝙖𝙘𝙚 𝙥𝙖𝙧𝙖 𝙖𝙘𝙤𝙧𝙩𝙖`
-let json = await (await fetch(`https://api.xteam.xyz/shorturl/tinyurl?url=${text}&apikey=cb15ed422c71a2fb`)).json()
-if (!json.status) throw json
-let hasil = `✅ 𝑺𝒆 𝒓𝒆𝒂𝒍𝒊𝒛𝒐  𝒄𝒐𝒏 𝒆́𝒙𝒊𝒕𝒐\n\n𝑬𝒏𝒍𝒂𝒄𝒆𝒔 𝒅𝒆 𝒂𝒏𝒕𝒆𝒔\n*${json.result}*`.trim()   
-m.reply(hasil)
-}
+let handler = async(m, { conn, args, text }) => {
+if (!text) throw '* 𝙔 𝙚𝙡 𝙚𝙣𝙡𝙖𝙘𝙚?*\n*𝙞𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙣𝙡𝙖𝙘𝙚 𝙥𝙖𝙧𝙖 𝙖𝙘𝙤𝙧𝙩𝙖*'
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()  
+if (!shortUrl1) throw `*[❗] ᴇʀʀᴏʀ, ᴄᴏᴍᴘʀᴜᴇʙᴇ  ǫᴜᴇ ᴇʟ  ᴛᴇxᴛᴏ ɪɴɢʀᴇsᴀᴅᴏ sᴇᴀ ᴜɴ ᴛᴇxᴛᴏ ᴇ ɪɴᴛᴇɴᴛᴇ ᴅᴇ ɴᴜᴇᴠᴏ*`
+let done = `*✅ sᴇ ʀᴇᴀʟɪᴢᴏ ᴄᴏɴ ᴇxɪᴛᴏ!!*\n\n*ᴇɴʟᴀᴄᴇ ᴅᴇ ᴀɴᴛᴇs:*\n${text}\n*ʟɪɴᴋ ᴀᴄᴏʀᴛᴀᴅᴏ:*\n${shortUrl1}`.trim()   
+m.reply(done)}
 handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
 handler.tags = ['tools']
 handler.command = /^(tinyurl|short|acortar|corto)$/i
@@ -14,3 +13,8 @@ handler.limit = 1
 handler.register = true
 handler.fail = null
 export default handler
+
+
+
+
+
---
+++ plugins/herramientas-chatGPT.js
@@ -0,0 +1,16 @@
+import fetcc from 'node-fetch'
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw `[❗] ɪɴɢʀᴇsᴇ ᴜɴᴀ ᴘᴇᴛɪᴄɪᴏɴ ᴏ ᴜɴᴀ ᴏʀᴅᴇɴ  ᴘᴀʀᴀ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇʟ ᴄʜᴀᴛɢᴘᴛ\n\n❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇ ʏ ᴏʀᴅᴇɴᴇs\n ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix\n${usedPrefix + command} Codigo en JS para un juego de cartas`
+try {
+m.reply('*[❗] ᴇsᴘᴇʀᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴇɴ  ʟᴏ ǫᴜᴇ  ᴍᴀɴᴅᴏ ʟᴏ ǫᴜᴇ  ᴍᴇ ᴘɪᴅɪᴏ*')
+let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
+let hasil = await tiores.json()
+m.reply(`${hasil.result}`.trim())
+} catch {
+throw `*[❗] ᴇʀʀᴏʀ, ᴠᴜᴇʟᴠᴀ ᴀʟ ɪɴᴛᴇɴᴛᴀʀʟᴏ*`
+}}
+handler.command = ['openai', 'chatgpt', 'ia', 'robot']
+export default handler
+
+ 
+
