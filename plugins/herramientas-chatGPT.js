c033ca8a7110a095a6c6b5363e0822ee0f5c6faf
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-14 21:16:38 -0300

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,4 +1,4 @@
-import fetcc from 'node-fetch'
+import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `[❗] ɪɴɢʀᴇsᴇ ᴜɴᴀ ᴘᴇᴛɪᴄɪᴏɴ ᴏ ᴜɴᴀ ᴏʀᴅᴇɴ  ᴘᴀʀᴀ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇʟ ᴄʜᴀᴛɢᴘᴛ\n\n❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇ ʏ ᴏʀᴅᴇɴᴇs\n ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix\n${usedPrefix + command} Codigo en JS para un juego de cartas`
 try {
