c72910fc7f9df4a07b494d3bba9161174ad86292
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-16 10:28:48 -0300

    Update herramientas-dallE.js

--- plugins/herramientas-dallE.js
+++ plugins/herramientas-dallE.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] ɪɴɢʀᴇsᴇ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ᴄʀᴇᴀʀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ʏ ᴀsɪ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇ ᴅᴀʟʟ-ᴇ*\n\n*❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇs*\n*${usedPrefix + command} gatitos llorando*\n*${usedPrefix + command} hatsune miku beso*`
+if (!text) throw `*[❗] ɪɴɢʀᴇsᴇ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ᴄʀᴇᴀʀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ʏ ᴀsɪ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇ ᴅᴀʟʟ-ᴇ*\n\n*❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇs*\n*${usedPrefix + command} gatitos llorando*\n*${usedPrefix + command} Gata beso*`
 try {
 m.reply('*[❗] ᴇsᴘᴇʀᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴇɴ ʟᴏ ǫᴜᴇ ᴍᴀɴᴅᴏ ʟᴏ ǫᴜᴇ ᴍᴇ ᴘɪᴅɪᴏ*')
 let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
