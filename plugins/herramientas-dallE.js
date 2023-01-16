ffd2f1b81845d740004aed29e9cf31675c769905
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-16 10:21:57 -0300

    Update herramientas-dallE.js

--- plugins/herramientas-dallE.js
+++ plugins/herramientas-dallE.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] ɪɴɢʀᴇsᴇ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ᴄʀᴇᴀʀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ʏ ᴀsɪ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇ ᴅᴀʟʟ-ᴇ*\n\n*—◉ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇs*\n*◉ ${usedPrefix + command} gatitos llorando*\n*◉ ${usedPrefix + command} hatsune miku beso*`
+if (!text) throw `*[❗] ɪɴɢʀᴇsᴇ ᴜɴ ᴛᴇxᴛᴏ ᴘᴀʀᴀ ᴄʀᴇᴀʀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ʏ ᴀsɪ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇ ᴅᴀʟʟ-ᴇ*\n\n*❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇs*\n*${usedPrefix + command} gatitos llorando*\n*${usedPrefix + command} hatsune miku beso*`
 try {
-m.reply('*[❗] ᴇsᴘᴇʀᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴇɴ ʟᴏs ǫᴜᴇ ᴍᴀɴᴅᴏ ʟᴏs ǫᴜᴇᴇ ᴘɪᴅɪ*')
+m.reply('*[❗] ᴇsᴘᴇʀᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴇɴ ʟᴏ ǫᴜᴇ ᴍᴀɴᴅᴏ ʟᴏ ǫᴜᴇ ᴍᴇ ᴘɪᴅɪᴏ*')
 let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
 await conn.sendFile(m.chat, tiores.data, null, null, m)
 } catch {
