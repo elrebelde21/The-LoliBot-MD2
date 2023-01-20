0f34b07e8046767371852cde2930d26a66c07f5b
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-20 20:41:05 -0300

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -1,20 +1,24 @@
-import fs from 'fs'
-let handler = async (m, { conn, text } ) => {
-let chatsall = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
-for (let id of chatsall) { 
-await delay(5 * 5000)
-conn.sendButton(id, `*┌───⊷ 𝘾𝙤𝙢𝙪𝙣𝙞𝙘𝙖𝙙𝙤*\n*┆*\n*┆💌* ${text}\n*┆*\n*╰──────────────────*`, '✅ *𝑪𝒐𝒎𝒖𝒏𝒊𝒄𝒂𝒅𝒐 𝒐𝒇𝒊𝒄𝒊𝒂𝒍*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, {
-contextInfo: { externalAdReply: {
-title: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',
-body: '𝑺𝒖𝒑𝒆𝒓 𝒃𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑', 
-sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`, 
-thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})}
-m.reply(`${iig} ✅ *𝑬𝒍 𝒎𝒆𝒏𝒔𝒂𝒋𝒆 𝒇𝒖𝒆 𝒆𝒏𝒗𝒊𝒂𝒅𝒐 𝒂 ${chatsall.length} 𝑪𝒉𝒂𝒕 𝒕𝒐𝒕𝒂𝒍𝒆𝒔*\n*𝑬𝒔 𝒑𝒐𝒔𝒊𝒃𝒍𝒆 𝒒𝒖𝒆 𝒏𝒐 𝒔𝒆 𝒉𝒂𝒚𝒂 𝒆𝒏𝒗𝒊𝒂𝒅𝒐 𝒂 𝒕𝒐𝒅𝒐𝒔 𝒍𝒐𝒔 𝑪𝒉𝒂𝒕𝒔 𝑷𝒓𝒊𝒗𝒂𝒅𝒐𝒔. 𝑫𝒊𝒔𝒄𝒖𝒍𝒑𝒆.*`)
+import { randomBytes } from 'crypto'
+
+let handler = async (m, { conn, text }) => {
+  let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `*[❕] ᴍᴇɴsᴀᴊᴇ ᴇɴᴠɪᴀᴅᴏ ᴀ ${chats.length} ᴄʜᴀᴛs ᴛᴏᴛᴀʟᴇs*`, m)
+  await delay(5 * 5000)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? `*𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ✅*\n` + teks : `*𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ✅*\n` + teks + '\n' + readMore + '\n\n' + wm), true).catch(_ => _)
+  m.reply('se envio con éxitos ✅️')
 }
 handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(comunicar|comunicado|broadcastall|bc)$/i
-handler.rowner = true
-handler.exp = 500
+
+handler.owner = true
+
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
