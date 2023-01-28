ac415fdbad89dba598781d9e97ba2f0f4d0035aa
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-28 17:57:39 -0300

    Update herramientas-spamwa.js

--- plugins/herramientas-spamwa.js
+++ plugins/herramientas-spamwa.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-await delay(5000)
+let time = global.db.data.users[m.sender].lastrob + 7200000
+if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️ ESPERA ${msToTime(time - new Date())}\n NO USER ESTE COMANDO CON UN MAL USO\n (wey por que no van vos hacerle spam a esta persona???? y no me use a mi para tu pinche pendejada)*`
 let [nomor, pesan, jumlah] = text.split('|')
 if (!nomor) throw `${mg}𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒆𝒍 𝒏𝒖́𝒎𝒆𝒓𝒐 𝒑𝒂𝒓𝒂 𝒆𝒍 𝒔𝒑𝒂𝒎\n*❊ ${usedPrefix + command} numero|texto|cantidad*\n𝑬𝒋𝒆𝒎𝒑𝒍𝒐\n*❊ ${usedPrefix + command} 999999999999|Holaaa|35*`
 if (!pesan) throw `${mg}𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝒆𝒍 𝒏𝒖́𝒎𝒆𝒓𝒐 𝒑𝒂𝒓𝒂 𝒆𝒍 𝒔𝒑𝒂𝒎\n*❊ ${usedPrefix + command} numero|texto|cantidad*\n𝑬𝒋𝒆𝒎𝒑𝒍𝒐\n*❊ ${usedPrefix + command} 999999999999|Holaaa|35*`
@@ -16,8 +17,9 @@ await delay(10000)
 for (let i = fixedJumlah; i > 1; i--) {
 await delay(10000)
 if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
-
-}}
+}
+global.db.data.users[m.sender].lastrob = new Date * 1
+}
 handler.help = ['spamwa <number>|<mesage>|<no of messages>']
 handler.tags = ['General']
 handler.command = /^spam(wa)?$/i
@@ -29,3 +31,13 @@ handler.limit = 280
 handler.register = true
 export default handler 
 const delay = time => new Promise(res => setTimeout(res, time))
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+return hours + " Hora(s) " + minutes + " Minuto(s)"}
