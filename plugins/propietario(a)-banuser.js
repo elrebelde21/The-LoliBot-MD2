3d6a450fea96871cfc915711865db1b512a5c366
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-28 18:53:28 -0300

    Add files via upload

--- plugins/propietario(a)-banuser.js
+++ plugins/propietario(a)-banuser.js
@@ -1,12 +1,13 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let BANtext = `𝑬𝒕𝒊𝒒𝒖𝒆𝒕𝒆 𝒂 𝒂𝒍𝒈𝒖𝒊𝒆𝒏 𝒑𝒂𝒓𝒂 𝒃𝒂𝒏𝒆𝒂𝒓\n𝑬𝒋𝒆𝒎𝒑𝒍𝒐:*\n*${usedPrefix + command} @${global.suittag}*`
+let BANtext = `𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙚 𝙖 𝙖𝙡𝙜𝙪𝙞𝙚𝙣 𝙥𝙖𝙧𝙖 𝙗𝙖𝙣𝙚𝙖𝙧\n𝙚𝙟𝙚𝙢𝙥𝙡𝙤:*\n*${usedPrefix + command} @${global.suittag}*`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext)})
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 else who = m.chat
 let users = global.db.data.users
 users[who].banned = true
-m.reply('*𝑬𝒍 𝒖𝒔𝒖𝒂𝒓𝒊𝒐 𝒇𝒖𝒆 𝒃𝒂𝒏𝒆𝒂𝒅𝒐(𝒂) 🙀\n𝑵𝒐 𝒑𝒐𝒅𝒓𝒂 𝒖𝒔𝒂𝒓 𝒂 ${lb}')    }
+let ban = './media/baneado.mp3'
+conn.sendFile(m.chat, ban, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}  
 handler.command = /^banuser$/i
 handler.rowner = true
 export default handler
