3470d677954b0502811e2fd76eb6c4c219a749fd
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-31 18:03:37 -0300

    Update _antipv.js

--- plugins/_antipv.js
+++ plugins/_antipv.js
@@ -1,3 +1,5 @@
+//este antiprivado 2 todavía está el desarrollo.
+
 export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
