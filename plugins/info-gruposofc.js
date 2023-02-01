fdf8002f5c38e7ae4a3ac54139e9dae6208a6d17
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-01 10:28:04 -0300

    Add files via upload

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,28 +1,24 @@
-let media = './media/menus/Menuvid3.mp4'
 let handler = async (m, { conn, command }) => {
-let str = `
-✨ 𝘽𝙞𝙚𝙣𝙫𝙚𝙣𝙞𝙙𝙤 𝙖 𝙡𝙤𝙨 𝙜𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- 𝙏𝙚 𝙞𝙣𝙫𝙞𝙩𝙤 𝙖 𝙦𝙪𝙚 𝙩𝙚 𝙪𝙣𝙖𝙨 𝙖 𝙡𝙖 𝙘𝙤𝙢𝙪𝙣𝙞𝙙𝙖𝙙 𝙩𝙝𝙚 𝙡𝙤𝙡𝙞𝙗𝙤𝙩-𝙈𝘿✨ 𝙪𝙣 𝙗𝙪𝙚𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤 𝙚 𝙞𝙣𝙩𝙚𝙧𝙖𝙘𝙘𝙞𝙤𝙣𝙖 𝙘𝙤𝙣 𝙣𝙤𝙨𝙤𝙩𝙧𝙤𝙨. 😸
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-𓃠 *Versión de ${lb}*
-➥ ${vs}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ ➤ 𝙂𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 𝙙𝙚𝙡 𝘽𝙤𝙩:
+  
+let grupos = `✨ 𝘽𝙞𝙚𝙣𝙫𝙚𝙣𝙞𝙙𝙤 𝙖 𝙡𝙤𝙨 𝙜𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨
+
+𝙏𝙚 𝙞𝙣𝙫𝙞𝙩𝙤 𝙖 𝙦𝙪𝙚 𝙩𝙚 𝙪𝙣𝙖𝙨 𝙖 𝙡𝙤𝙨 𝙜𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 𝙥𝙖𝙧𝙖 𝙥𝙖𝙨𝙖𝙧 𝙪𝙣 𝙧𝙖𝙩𝙤 𝙖𝙜𝙧𝙖𝙙𝙖𝙗𝙡𝙚 𝙪𝙨𝙖𝙣𝙙𝙤 𝙚𝙡 𝘽𝙤𝙩 𝙤 𝙪𝙣 𝙗𝙪𝙚𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤 𝙚 𝙞𝙣𝙩𝙚𝙧𝙖𝙘𝙘𝙞𝙤𝙣𝙖 𝙘𝙤𝙣 𝙣𝙤𝙨𝙤𝙩𝙧𝙤𝙨 😸
+
+➤ 𝙂𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 𝙙𝙚𝙡 𝘽𝙤𝙩:
 1) *${nn}*\n
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
 2) *${nnn}*\n
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *Grupo del Colaboracion ${lb} con GataBot-MD*
+
+➤ 𝙂𝙧𝙪𝙥𝙤 𝙙𝙚𝙡 𝘾𝙤𝙡𝙖𝙗𝙤𝙧𝙖𝙘𝙞𝙤𝙣 𝙇𝙤𝙡𝙞𝙗𝙤𝙩 𝙘𝙤𝙣 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿
  *${nnnt}*\n
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *Grupo del col ² (aquí se permití todo los comando del bot)*
+ 
+➤ 𝙂𝙧𝙪𝙥𝙤 𝙙𝙚𝙡 𝙘𝙤𝙡 ² (𝙖𝙦𝙪𝙞 𝙨𝙚 𝙥𝙚𝙧𝙢𝙞𝙩𝙞 𝙩𝙤𝙙𝙤 𝙡𝙤𝙨 𝙘𝙤𝙢𝙖𝙣𝙙𝙤 𝙙𝙚𝙡 𝙗𝙤𝙩)
 *${nnntt}*\n
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- *infomarte sobre la nueva actualización del bot*
+
+➤ 𝙞𝙣𝙛𝙤𝙢𝙖𝙧𝙩𝙚 𝙨𝙤𝙗𝙧𝙚 𝙡𝙖 𝙣𝙪𝙚𝙫𝙖 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙘𝙞𝙤𝙣 𝙙𝙚𝙡 𝙗𝙤𝙩
  *${nna}*\n
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*𝙂𝙧𝙪𝙥𝙤𝙨 𝙙𝙚𝙡 𝙖𝙮𝙪𝙙𝙖𝙧 𝙨𝙤𝙗𝙧𝙚 𝙚𝙡 𝙗𝙤𝙩*
+ 
+➤ 𝙂𝙧𝙪𝙥𝙤𝙨 𝙙𝙚𝙡 𝙖𝙮𝙪𝙙𝙖𝙧 𝙨𝙤𝙗𝙧𝙚 𝙚𝙡 𝙗𝙤𝙩
  *${nnnttt}*\n 
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 𝙜𝙧𝙪𝙥𝙤 𝙙𝙚 𝙖𝙢𝙞𝙨𝙩𝙖𝙙𝙚𝙨 𝙙𝙤𝙣𝙙𝙚 𝙚𝙨𝙩𝙖 𝙚𝙡 𝙗𝙤𝙩
@@ -33,24 +29,16 @@ https://chat.whatsapp.com/DCJclB8oBAPIAoleUtNEaN
 *꧁⁣𓆩Aᴹᴵᴳᴼˢ Cᴴᴬᵀˢ Y Aᴾᴼᴿᵀᴱ*
 https://chat.whatsapp.com/JZaD3sfNoVW4JvaoQ4uVwF
 
-𝐚𝐪𝐮𝐢 𝐩𝐮𝐞𝐝𝐞 𝐦𝐚𝐧𝐝𝐚 𝐭𝐮 𝐞𝐧𝐥𝐚𝐜𝐞 𝐲 𝐭𝐚𝐦𝐛𝐢𝐞́𝐧 𝐬𝐢 𝐪𝐮𝐢𝐞𝐫𝐞 𝐮𝐧 𝐛𝐨𝐭 𝐩𝐚𝐫𝐚 𝐭𝐮 𝐠𝐫𝐮𝐩𝐨 𝐩𝐨𝐧𝐞𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 .𝐣𝐨𝐢𝐧 (𝐦𝐚𝐬 𝐞𝐧𝐥𝐚𝐜𝐞)
-
 *⇶⃤꙰𝑬𝒏𝒍𝒂𝒄𝒆 𝒍𝒐𝒍𝒊𝒃𝒐𝒕ꦿ⃟⃢*
 *${nnnttt}*\n
 
+𝐚𝐪𝐮𝐢 𝐩𝐮𝐞𝐝𝐞 𝐦𝐚𝐧𝐝𝐚 𝐭𝐮 𝐞𝐧𝐥𝐚𝐜𝐞 𝐲 𝐭𝐚𝐦𝐛𝐢𝐞́𝐧 𝐬𝐢 𝐪𝐮𝐢𝐞𝐫𝐞 𝐮𝐧 𝐛𝐨𝐭 𝐩𝐚𝐫𝐚 𝐭𝐮 𝐠𝐫𝐮𝐩𝐨 𝐩𝐨𝐧𝐞𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 .𝐣𝐨𝐢𝐧 (𝐦𝐚𝐬 𝐞𝐧𝐥𝐚𝐜𝐞)
+
 *꧁⃢⃟҉💫🌺ঔঔ𝔼ℕ𝕃𝔸ℂ𝔼𝕊 𝕃𝔸 𝕁𝔼𝔽𝔸✨*
 https://chat.whatsapp.com/JegOp8NUSMd0vn75s4hkaj
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- 𝙨𝙞 𝙦𝙪𝙞𝙚𝙧𝙚 𝙦𝙪𝙚 𝙩𝙪 𝙜𝙧𝙪𝙥𝙤 𝙖𝙥𝙖𝙧𝙚𝙯𝙘𝙖 𝙖𝙘𝙖 𝙝𝙖𝙗𝙡𝙖 𝙘𝙤𝙣 𝙚𝙡 𝙖𝙙𝙢𝙞𝙣 𝙤𝙛𝙘 𝙙𝙚𝙡 𝙗𝙤𝙩
-`.trim()
-  
-conn.sendHydrated(m.chat, str, `` + wm, media, 'https://github.com/elrebelde21/The-LoliBot-MD', '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫', null, null, [
-['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 ✅', '.cuentasgb'],
-['🎁 𝘿𝙤𝙣𝙖𝙧 ', '.donar'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)}
-
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
+conn.sendButton(m.chat, grupos, `𝙨𝙞 𝙦𝙪𝙞𝙚𝙧𝙚 𝙦𝙪𝙚 𝙩𝙪 𝙜𝙧𝙪𝙥𝙤 𝙖𝙥𝙖𝙧𝙚𝙯𝙘𝙖 𝙖𝙘𝙖 𝙝𝙖𝙗𝙡𝙖 𝙘𝙤𝙣 𝙚𝙡 𝙖𝙙𝙢𝙞𝙣 𝙤𝙛𝙘 𝙙𝙚𝙡 𝙗𝙤𝙩\n\n𓃠 ${lb} `, [
+['✨ 𝙈𝙀𝙉𝙐 ✨️', '.menu']], m)
+}
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupgb$/i
-handler.exp = 33
-
 export default handler
