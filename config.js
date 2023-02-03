a8c27d57b314f5ce72da3cd3b55ba9b85da29074
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-03 09:42:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -201,6 +201,7 @@ global.lolkeysapi = ['BrunoSobrino']
 global.imagen1 = fs.readFileSync('./media/menus/Menu3.jpg')
 global.imagen2 = fs.readFileSync('./media/menus/img1.jpg')
 global.imagen3 = fs.readFileSync('./media/menus/img2.jpg')
+global.imagen4 = fs.readFileSync('./media/menus/img3.jpg')
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
