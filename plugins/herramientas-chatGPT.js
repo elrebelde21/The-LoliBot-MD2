75dde1b13cc07c35887df6b6fa1b1128c58154fb
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-06 14:24:46 -0300

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `[❗] ɪɴɢʀᴇsᴇ ᴜɴᴀ ᴘᴇᴛɪᴄɪᴏɴ ᴏ ᴜɴᴀ ᴏʀᴅᴇɴ  ᴘᴀʀᴀ ᴜsᴀʀ ʟᴀ ғᴜɴᴄɪᴏɴ ᴅᴇʟ ᴄʜᴀᴛɢᴘᴛ\n\n❏ ᴇᴊᴇᴍᴘʟᴏ ᴅᴇ ᴘᴇᴛɪᴄɪᴏɴᴇ ʏ ᴏʀᴅᴇɴᴇs\n ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix\n${usedPrefix + command} Codigo en JS para un juego de cartas`
 try {
-m.reply('*[❗] ᴇsᴘᴇʀᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴇɴ  ʟᴏ ǫᴜᴇ  ᴍᴀɴᴅᴏ ʟᴏ ǫᴜᴇ  ᴍᴇ ᴘɪᴅɪᴏ*')
+await conn.sendPresenceUpdate('composing', m.chat)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
 m.reply(`${hasil.result}`.trim())
