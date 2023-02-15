7c7e03b634adf2137fa22d19cecc46f01df87b31
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-15 19:38:26 -0300

    Update info-grupos-lista.js

--- plugins/info-grupos-lista.js
+++ plugins/info-grupos-lista.js
@@ -1,12 +1,10 @@
 let handler = async (m, { conn }) => {
-    let txt = ''
-    let vn = './media/listas.mp3'
-    const groupsIn = chats.filter(([jid]) => jid.endsWith('@g.us'))
-    for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('https://chat.whatsapp.com/') + conn.groupInviteCode(jid) && chat.isChats)) txt += `${await conn.getName(jid)}\n✳ ${jid} [${chat?.metadata?.read_only ? '❌ *No estoy aquí*' : '✅ *Si estoy aquí*'}]\n\n`
-    m.reply(`${lb} Esta en estos grupos:\n *✦ Total de Grupos:* *${groupsIn.length}*
-    
+let txt = ''
+let vn = './media/listas.mp3'
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '❌ *No estoy aquí*' : '✅ *Si estoy aquí'}]\n\n`
+m.reply(`*${lb} Esta en estos grupos::*
 ${txt}
-`.trim())   
+`.trim())
 conn.sendFile(m.chat, vn, 'listas.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
 handler.help = ['groups', 'grouplist']
