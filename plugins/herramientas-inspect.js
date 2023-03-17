606b1a6dafb724cedf10948e03b08b0c13a4aea9
CuriosityBot-MD <119384234+Azami19@users.noreply.github.com>
2023-03-17 15:49:10 -0600

    Update herramientas-inspect.js

--- plugins/herramientas-inspect.js
+++ plugins/herramientas-inspect.js
@@ -17,9 +17,9 @@ let handler = async (m, { conn, text }) => {
 `
 	await conn.reply(m.chat, groupinfo, m)
 	const botones = [
-{index: 1, urlButton: {displayText: `Copiar Descripción ⚡`, url: `https://www.whatsapp.com/otp/copy/${data.desc}`}},
+{index: 1, urlButton: {displayText: `Copiar Descripción 🍧`, url: `https://www.whatsapp.com/otp/copy/${data.desc}`}},
 ]
-await conn.sendMessage(m.chat, { text: `*┏━━━━━━━━━━━━━━┓*\n┃¿Desa copiar la descripción? •🌷\n*┗━━━━━━━━━━━━━━┛*`, templateButtons: botones, footer: wm })
+await conn.sendMessage(m.chat, { text: `*┏━━━━━━━━━━━━━━┓*\n┃¿Desa copiar la desc? •🌷\n*┗━━━━━━━━━━━━━━┛*`, templateButtons: botones, footer: wm })
 }
 handler.command = /^(inspect)$/i
 
