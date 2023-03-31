fe99186d061ac38dd7dc97d29cf897796fd30f9a
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-03-31 10:00:22 -0300

    Update herramientas-chatGPT2.js

--- plugins/herramientas-chatGPT2.js
+++ plugins/herramientas-chatGPT2.js
@@ -3,8 +3,7 @@
 /*                                                       */ 
 /*       |- [ ⚠ ] - CREDITOS DEL CODIGO - [ ⚠ ] -|      */
 /*     —◉ DESAROLLADO POR OTOSAKA:                       */
-/*     ◉ Otosaka (https://github.com/6otosaka9)          */
-/*     ◉ Número: wa.me/51993966345                       */
+/*     ◉ Otosaka (https://github.com/6otosaka9)          */                    */
 /*                                                       */
 /*     —◉ FT:                                            */
 /*     ◉ BrunoSobrino (https://github.com/BrunoSobrino)  */
