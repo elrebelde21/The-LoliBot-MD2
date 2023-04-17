9b53eb7dfc48555f852d3562efe700caae50f3e6
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-17 18:39:03 -0300

    Update buscador-lyrics.js

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -3,7 +3,8 @@ import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-if (!teks) throw `𝙌𝙪𝙚 𝙚𝙨𝙩𝙖́ 𝙗𝙪𝙨𝙘𝙖𝙣𝙙𝙤❓️\n𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙣𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙣𝙖 𝙘𝙖𝙣𝙘𝙞𝙤́𝙣 𝙥𝙖𝙧𝙖 𝙤𝙗𝙩𝙚𝙣𝙚𝙧 𝙡𝙖 𝙡𝙚𝙩𝙧𝙖\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤\n*${usedPrefix + command} Runaway*`
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+if (!teks) throw `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉𝘼 𝘾𝘼𝙉𝘾𝙄𝙊𝙉 𝙋𝘼𝙍𝘼 𝙊𝘽𝙏𝙀𝙉𝙀𝙍 𝙇𝘼 𝙇𝙀𝙏𝙍𝘼\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Runaway*\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙎𝙊𝙉𝙂 𝙏𝙊 𝙂𝙀𝙏 𝙏𝙃𝙀 𝙇𝙔𝙍𝙄𝘾𝙎\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Billie Eilish bored*`
 const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
 let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
 title: result.author + result.title}))
@@ -11,9 +12,7 @@ if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.thumbnail.genius) throw json
 
-
-let letratexto =`
-𝙏𝙞́𝙩𝙪𝙡𝙤
+await conn.reply(m.chat, `𝙏𝙞́𝙩𝙪𝙡𝙤
 💚 *${result.title}*
 
 𝘼𝙪𝙩𝙤𝙧(𝘼)
@@ -24,13 +23,26 @@ ${result.lyrics}
 
 
 𝙀𝙣𝙡𝙖𝙘𝙚
+🧡 *${result.link}*`, fkontak,  m)
+/*let letratexto =`
+𝙏𝙄𝙏𝙐𝙇𝙊 | 𝙏𝙄𝙏𝙇𝙀 
+💚 *${result.title}*
+
+𝘼𝙐𝙏𝙊𝙍(𝘼) | 𝘼𝙐𝙏𝙃𝙊𝙍
+💜 *${result.author}*
+
+
+${result.lyrics}
+
+
+𝙀𝙉𝙇𝘼𝘾𝙀 | 𝙐𝙍𝙇
 🧡 *${result.link}*
 `.trim()
 conn.sendHydrated(m.chat, letratexto, wm, json.thumbnail.genius, null, null, null, null, [
-['𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 🚀', `/play ${text}`],
-['𝙈𝙚𝙣𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖𝙨 🔎', '#buscarmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', '/menu']
-], m)
+['𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 | 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 🚀', `/play ${text}`],
+['𝙈𝙚𝙣𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖𝙨 | 𝙎𝙚𝙖𝙧𝙘𝙝𝙚𝙨 🔎', '#buscarmenu'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m)*/
 }
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
