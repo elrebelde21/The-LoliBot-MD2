510eaf24662f9dfd96f1544044341d0b1a0d6232
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-04-28 18:01:59 -0300

    Add files via upload

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,15 +1,9 @@
-import fs from 'fs'
 let handler = async (m, { conn, args }) => {
 let group = m.chat
-conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-body: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`}}})   
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+conn.reply(m.chat, link, m, {detectLink: true})
+//conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
 }
-handler.help = ['linkgroup']
-handler.tags = ['group']
 handler.command = /^link(gro?up)?$/i
 handler.group = true
 handler.botAdmin = true
