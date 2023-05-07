602f9a8d734f89a0a4f8aeb195a22665e68c07aa
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-07 11:41:18 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -23,7 +23,7 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => imagen1[1]) 
 let teksbio = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤* 
-*Wa.me/59822228462 (no bot)*
+*Wa.me/972529277518 (no bot)*
 *Wa.me/573183650526 (bot)*
 *wa.me/527822175745 (bot ²)*
 *wa.me/972529277516 (bot ³)*
