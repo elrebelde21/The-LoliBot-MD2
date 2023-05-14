f5d9669c29e2fad0418e3a090ae034beb5fd37f6
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-14 20:49:48 -0300

    Add files via upload

--- plugins/propietario(a)-bcgc.js
+++ plugins/propietario(a)-bcgc.js
@@ -1,14 +1,27 @@
 import { randomBytes } from 'crypto'
-
-let handler = async (m, { conn, text }) => {
-  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-  let teks = text ? text : cc.text
-  conn.reply(m.chat, `[❕] ᴍᴇɴsᴀᴊᴇ ᴇɴᴠɪᴀᴅᴏ ᴀ ${groups.length} ɢʀᴜᴘᴏs`, m)
-  await delay(5 * 5000)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? `*𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ✅*\n` + teks : `*𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 ✅*\n` + teks + '\n' + readMore + '\n\n' + wm), true).catch(_ => _)
-  m.reply('se envio con éxitos ✅️')
+let handler = async (m, { conn, command, participants, usedPrefix, text }) => {
+if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
+let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks2 = text ? text : cc2.text 
+let d = new Date(new Date + 3600000)
+let locale = lenguajeGB.lenguaje()
+let dia = d.toLocaleDateString(locale, { weekday: 'long' })
+let fecha = d.toLocaleDateString(lenguajeGB.lenguaje(), { day: 'numeric', month: 'numeric', year: 'numeric' })
+let mes = d.toLocaleDateString(lenguajeGB.lenguaje(), { month: 'long' })
+let año = d.toLocaleDateString(lenguajeGB.lenguaje(), { year: 'numeric' })
+let tiempo = d.toLocaleString('es-CO', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+let groups = Object.keys(await conn.groupFetchAllParticipating())
+let usersTag = participants.map(u => conn.decodeJid(u.id))
+let readMS = String.fromCharCode(8206).repeat(850)
+await m.reply(lenguajeGB.smsChatGP1())
+for (let id of groups) {  
+let infoGP = lenguajeGB.smsChatGP2(readMS, dia, mes, año, fecha, tiempo)  
+await delay(2 * 2000)
+await conn.reply(id, infoGP + teks2, { mentions: usersTag }, { quoted: m })         
 }
+let totalGP = groups.length
+await m.reply(lenguajeGB.smsChatGP3(totalGP))
+}     
 handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
@@ -17,8 +30,4 @@ handler.owner = true
 
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+  
