cb94539e7fb986bf635ee6ca8df0f02a9fc8c8cf
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-14 15:53:27 -0300

    Add files via upload

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -1,20 +1,43 @@
-import fs from 'fs'
-let handler = async (m, { conn, text }) => {
-let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
-for (let id of chats) { 
-await delay(5 * 5000)
-conn.sendButton(id, `*┌───⊷ 𝘾𝙤𝙢𝙪𝙣𝙞𝙘𝙖𝙙𝙤*\n*┆*\n*┆💌* ${text}\n*┆*\n*╰──────────────────*`, '✅ *𝑪𝒐𝒎𝒖𝒏𝒊𝒄𝒂𝒅𝒐 𝒐𝒇𝒊𝒄𝒊𝒂𝒍*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, {
-contextInfo: { externalAdReply: {
-title: '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫',
-body: '𝑺𝒖𝒑𝒆𝒓 𝒃𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑', 
-sourceUrl: `https://github.com/elrebelde21/The-LoliBot-MD`, 
-thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})}
-m.reply(`${iig} ✅ *𝑬𝒍 𝒎𝒆𝒏𝒔𝒂𝒋𝒆 𝒇𝒖𝒆 𝒆𝒏𝒗𝒊𝒂𝒅𝒐 𝒂 ${chats.length} 𝑪𝒉𝒂𝒕𝒔 𝑷𝒓𝒊𝒗𝒂𝒅𝒐𝒔*\n*𝑬𝒔 𝒑𝒐𝒔𝒊𝒃𝒍𝒆 𝒒𝒖𝒆 𝒏𝒐 𝒔𝒆 𝒉𝒂𝒚𝒂 𝒆𝒏𝒗𝒊𝒂𝒅𝒐 𝒂 𝒕𝒐𝒅𝒐𝒔 𝒍𝒐𝒔 𝑪𝒉𝒂𝒕𝒔 𝑷𝒓𝒊𝒗𝒂𝒅𝒐𝒔. 𝑫𝒊𝒔𝒄𝒖𝒍𝒑𝒆.*`)
+import { randomBytes } from 'crypto'
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+  if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
+let cc3 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+let teks3 = text ? text : cc3.text  
+let chats = Object.keys(global.db.data.users).filter(user => user.endsWith('@s.whatsapp.net')) 
+await conn.reply(m.chat, lenguajeGB.smsBCMensaje2(), m)
+let start = new Date().getTime()
+let totalPri = 0
+for (let user of chats) {
+await new Promise(resolve => setTimeout(resolve, 2000)) // 2 segundos
+await delay(2 * 2000)
+await conn.reply(user, `${lenguajeGB.smsBCbot7()}\n\n` + teks3, null)
+totalPri++
+if (totalPri >= 3000) { 
+break
+}}   
+let end = new Date().getTime() 
+let time = Math.floor((end - start) / 1000)
+if (time >= 60) {
+let minutes = Math.floor(time / 60)
+let seconds = time % 60
+time = `${minutes} minutos y ${seconds} segundos`
+} else {
+time = `${time} segundos`
+}
+await m.reply(lenguajeGB.smsBCMensaje3(totalPri, time))
 }
 handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(comunicarpv|anunciopv|annunciopv|broadcastchats?|bcc(hats?)?)$/i
-handler.exp = 500
-handler.rowner = true
+handler.command = /^(broadcastchats?|bcc(hats?)?)$/i
+
+handler.owner = true
+
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+  
