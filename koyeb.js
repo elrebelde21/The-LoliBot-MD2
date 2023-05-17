224db07697256ca1372bd24198410be799b0d46e
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-17 11:33:25 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- koyeb.js
+++ koyeb.js
@@ -1,5 +1,5 @@
 FROM fedora:37
-
+ 
 RUN sudo dnf -y update &&\
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
