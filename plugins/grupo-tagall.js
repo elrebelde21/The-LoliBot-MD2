8f2be33670363fbd3e34b686062725f00a92e42e
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-05-20 20:33:16 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -16,6 +16,6 @@ conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', 
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
-//handler.admin = true
+handler.admin = true
 handler.group = true
 export default handler
