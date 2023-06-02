31c0eb6e1535d7f409e8ef4f02fa8d19e38d7fff
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-02 12:55:07 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -25,7 +25,8 @@ let teksbio = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤* 
 *Wa.me/5492266466080 (no bot)*
 *Wa.me/573183650526 (bot)*
-*wa.me/51955192184 (bot ²)*
+*wa.me/573106055136 +bot ²)*
+*wa.me/51955192184 (bot ³)*
 `
   let teks = ' '
 const sections = [
