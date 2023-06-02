225023e87854fc7a2246dc878b5fae85ac3bd054
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-06-02 14:30:52 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- plugins/propietario(a)-bcgc.js
+++ plugins/propietario(a)-bcgc.js
@@ -1,6 +1,7 @@
 import { randomBytes } from 'crypto'
 let handler = async (m, { conn, command, participants, usedPrefix, text }) => {
 if (!text && !m.quoted) return m.reply(lenguajeGB.smsBCMensaje(usedPrefix, command))        
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
 let cc2 = text ? m : m.quoted ? await m.getQuotedObj() : false || m
 let teks2 = text ? text : cc2.text 
 let d = new Date(new Date + 3600000)
@@ -13,12 +14,15 @@ let tiempo = d.toLocaleString('es-CO', { hour: 'numeric', minute: 'numeric', sec
 let groups = Object.keys(await conn.groupFetchAllParticipating())
 let usersTag = participants.map(u => conn.decodeJid(u.id))
 let readMS = String.fromCharCode(8206).repeat(850)
-await m.reply(lenguajeGB.smsChatGP1())
-for (let id of groups) {  
-let infoGP = lenguajeGB.smsChatGP2(readMS, dia, mes, año, fecha, tiempo)  
+await m.reply(lenguajeGB.smsChatGP1())   
+for (let i = 0; i < groups.length; i++) {
+const id = groups[i];
+const infoGP = lenguajeGB.smsChatGP2(readMS, dia, mes, año, fecha, tiempo)
+const delay = i * 4000 //4 seg
+setTimeout(async () => {
 await delay(2 * 2000)
-await conn.reply(id, infoGP + teks2, { mentions: usersTag }, { quoted: m })         
-}
+await conn.reply(id, infoGP + teks2, { mentions: usersTag }, { quoted: fkontak });
+}, delay)}        
 let totalGP = groups.length
 await m.reply(lenguajeGB.smsChatGP3(totalGP))
 }     
@@ -29,5 +33,8 @@ handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 handler.owner = true
 
 export default handler
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
   
+const delay = time => new Promise(res => setTimeout(res, time))
