14a152c8910adee14390d64b3cd968010b351f6a
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-08 23:21:59 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,13 +5,13 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/cuentasecundariaa/Bot.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/elrebelde21/The-LoliBot-MD.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
 }}
 handler.help = ['update']
-handler.tags = ['owner']
+handler.tags = ['owner'] 
 handler.command = /^update|actualizar$/i
 handler.rowner = true
 export default handler 
