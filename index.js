5b5f6545034efa1ca8eaeaab39daedf69750ebbe
LoliBot <107805973+elrebelde21@users.noreply.github.com>
2023-08-17 18:51:21 -0300

    𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 console.log('Iniciando 🚀🚀🚀') 
-import {join, dirname} from 'path';
-import {createRequire} from 'module';
-import {fileURLToPath} from 'url';
-import {setupMaster, fork} from 'cluster';
-import {watchFile, unwatchFile} from 'fs';
-import cfonts from 'cfonts';
-import {createInterface} from 'readline';
-import yargs from 'yargs';
+import { join, dirname } from "path";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { setupMaster, fork } from "cluster";
+import { watchFile, unwatchFile } from "fs";
+import cfonts from "cfonts";
+import chalk from "chalk";
+import { createInterface } from "readline";
+import yargs from "yargs";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const {name, author} = require(join(__dirname, './package.json'));
-const {say} = cfonts;
+const { name, author } = require(join(__dirname, "./package.json"));
+const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
 say('The Loli\nBot\nMD', {
@@ -20,52 +21,49 @@ gradient: ['red', 'magenta']})
 say(`Por LoliBot`, {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']})
+gradient: ['red', 'magenta']});
 
-let isRunning = false;
+var isRunning = false;
 /**
 * Start a js file
 * @param {String} file `path/to/file`
 */
 function start(file) {
-if (isRunning) return;
+if (isRunning) return
 isRunning = true;
-const args = [join(__dirname, file), ...process.argv.slice(2)];
-
+let args = [join(__dirname, file), ...process.argv.slice(2)]
+  
 setupMaster({
 exec: args[0],
-args: args.slice(1)});
-const p = fork();
-p.on('message', (data) => {
-//console.log('[RECEIVED]', data);
+args: args.slice(1),
+})
+let p = fork()
+p.on('message', data => {
+console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
 switch (data) {
 case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
 case 'uptime':
-p.send(process.uptime());
-break;
-}
-});
+p.send(process.uptime())
+break
+}})
 p.on('exit', (_, code) => {
-isRunning = false;
+isRunning = false
 console.error('⚠️ Error Inesperado ⚠️', code)
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-if (process.env.pm_id) {
-process.exit(1);
-} else {
-process.exit();
+if (code === 0) return
+watchFile(args[0], () => {
+unwatchFile(args[0])
+start(file)
+})
+})
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+if (!opts['test'])
+if (!rl.listenerCount()) rl.on('line', line => {
+p.emit('message', line.trim())
+})
+//console.log(p)
 }
-});
-const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test']) {
-if (!rl.listenerCount()) {
-rl.on('line', (line) => {
-p.emit('message', line.trim());
-});
-}}}
-start('main.js');
+start('main.js')
